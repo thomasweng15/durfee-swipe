82dd5e56e380f5d1d30c8a591f28b653cbf4a44a
Thomas Weng <thomas.weng11@gmail.com>
2014-10-27 13:58:28 -0400

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name durfeeswipeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the durfeeswipeApp
+ */
+angular.module('durfeeswipeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
