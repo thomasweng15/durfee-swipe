1f933682f61f479e60f5b13628b21bf840e80517
thomasweng15 <thomas.weng11@gmail.com>
2014-10-28 17:07:03 -0400

    Implemented basic search match and shopping bag display

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,11 +15,35 @@ angular.module('durfeeswipeApp')
       'Karma'
     ];
 
-    $scope.shoppingBag = [];
+    $scope.shoppingBag = {
+        totalPrice: 0.00,
+        items: []
+    };
     $scope.searchItem = "";
+    $scope.products = [
+      {
+        name: "chips",
+        price: 3.00
+      },
+      {
+        name: "soda",
+        price: 2.00
+      }
+    ];
 
     $scope.lookUpItem = function (searchItem) {
-    	// TODO Find matches for item
+      // TODO validation
+      for (var p in $scope.products) {
+        if ($scope.searchItem === $scope.products[p].name) {
+          $scope.addItemToBag($scope.products[p]);
+        }
+      }
     	$scope.searchItem = "";
+    };
+
+    $scope.addItemToBag = function (item) {
+      // TODO search through bag and +1 to item if it exists.
+      $scope.shoppingBag.items.push(item);
+      $scope.shoppingBag.totalPrice += item.price;
     };
   });
