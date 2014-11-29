4f0ed54a626af04a0a0a27a60497f0db4488acd3
thomasweng15 <thomas.weng11@gmail.com>
2014-11-29 00:01:52 -0500

    Wrote homepage and shopping bag e2e tests

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -2,27 +2,39 @@
 
 var ptor = protractor.getInstance();
 
+function submitSearch (query) {
+	element(by.id('product-search-box')).sendKeys(query);
+ 	element(by.id('product-search-submit')).click();
+};
+
+beforeEach(function () {
+	browser.get('/');
+});
+
+describe('Homepage', function () {
+
+	it('should have a title', function () {
+		expect(browser.getTitle()).toEqual('Durfee Swipe');
+	});
+});
+
 describe('Product Search', function () {
 	
-	browser.get('/');
-
 	it('should have a searchbox and submit button', function () {
-		expect(element(by.id('product-search-box')).isPresent()).toBe(true);
-		expect(element(by.id('product-search-submit')).isPresent()).toBe(true);
+		expect(element(by.id('product-search-box')).isDisplayed()).toBe(true);
+		expect(element(by.id('product-search-submit')).isDisplayed()).toBe(true);
 	});
 
 	it('should not add an invalid product to the shopping bag', function () {
 		// hardcoded invalid product
-		element(by.id('product-search-box')).sendKeys('invalid product');
-		element(by.id('product-search-submit')).click();
+		submitSearch('invalid product');
 		expect(element(by.id('shopping-bag-list')).isDisplayed()).toBe(false);
 		// TODO test that alert shows up
 	});
 
 	it('should add a valid product to the shopping bag', function () {
 		// hardcoded valid product
-		element(by.id('product-search-box')).sendKeys('Jalapeno Chips');
-		element(by.id('product-search-submit')).click();
+		submitSearch('Jalapeno Chips');
 		expect(element(by.id('shopping-bag-list')).isDisplayed()).toBe(true);
 	});
 
@@ -34,3 +46,29 @@ describe('Product Search', function () {
 	});
 });
 
+describe('Shopping Bag', function () {
+
+	it('should display total price when not empty', function () {
+		expect(element(by.id('bag-total-price')).isDisplayed()).toBe(false);
+		submitSearch('Jalapeno Chips');
+		expect(element(by.id('bag-total-price')).isDisplayed()).toBe(true);
+	});
+
+	it('should display the added item', function () {
+		submitSearch('Jalapeno Chips');
+		var items = element.all(by.repeater('item in srvc.model.shoppingBag.items'));
+		expect(items.count()).toBe(1);
+		expect(items.first().getText()).toContain('Jalapeno Chips');
+		// TODO test for item price, item count
+	});
+
+	it('should be able to remove items', function () {
+		submitSearch('Jalapeno Chips');
+		submitSearch('Jalapeno Chips');
+		var items = element.all(by.repeater('item in srvc.model.shoppingBag.items'));
+		items.first().element(by.css('.bag-remove-item')).click();
+		// TODO expect count to decrease
+		items.first().element(by.css('.bag-remove-item')).click();
+		expect(element(by.id('bag-total-price')).isDisplayed()).toBe(false);
+	});
+});
